035d33d1c1679df5635232e340204d4c262ed710
moochaee <marcusamad@live.com>
2024-10-02 07:19:41 +0000

    Wed, Oct 2, 2024, 5:19 PM +10:00

--- compare.c
+++ compare.c
@@ -2,10 +2,11 @@
 #include <cs50.h>
 
 int main(void)
+questions
 {
     int x = get_int("What's x? ");
     int y = get_int("What's y? ");
-
+comparisons
     if(x<y)
     {
     printf("x is less than y\n");
