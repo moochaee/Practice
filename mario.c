c77e3d394cb6e98d140c221c33979bcd596b84cb
moochaee <marcusamad@live.com>
2024-10-03 05:31:28 +0000

    Thu, Oct 3, 2024, 3:31 PM +10:00

---
+++ mario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+        // Prompt user for positive integer
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+
+    // Print an n-by-n grid of bricks
+    for (int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+}
