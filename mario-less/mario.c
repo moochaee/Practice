48659707dc764d516274f22cffbc24339412585a
moochaee <marcusamad@live.com>
2024-10-03 06:29:27 +0000

    Thu, Oct 3, 2024, 4:29 PM +10:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -17,19 +17,21 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         //Print row of space & bricks
-        print_row(i + 2 , i + 1);
+        print_row(i -1 , i + 1);
     }
 }
 
-void print_row(int space, int bricks)
+void print_row(int space , int bricks)
 {
-    for (int i = 0; i < bricks; i++)
-    {
-        printf("#");
-    }
+     //Print spaces
     for (int i = 0; i < space; i++)
     {
         printf(" ");
+    }
+    //Print bricks
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
     }
     printf("\n");
 }
