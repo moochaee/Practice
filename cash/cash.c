196f8a835e2f3b4f751745fa0cd0a17cd387cadc
moochaee <marcusamad@live.com>
2024-10-03 08:14:06 +0000

    Thu, Oct 3, 2024, 6:14 PM +10:00

--- cash/cash.c
+++ cash/cash.c
@@ -9,39 +9,25 @@ int main(void)
     int cents;
     do
     {
-    cents = get_int("Change owed ");
+        cents = get_int("Change owed: ");
     }
     while (cents < 0);
-    
-    printf("Quarters: %i\n", quarters);
 
+    // Calculate how many quarters you should give customer
+    int quarters = calculate_quarters(cents);
+
+    // Subtract the value of those quarters from cents
+    cents = cents - (quarters * 25);
 }
-    int calculate_quarters(int cents)
+
+int calculate_quarters(int cents)
 {
     // Calculate how many quarters you should give customer
     int quarters = 0;
     while (cents >= 25)
     {
         quarters++;
-        cents = cents -25;
+        cents = cents - 25;
     }
-        return quarters;
-
-
+    return quarters;
 }
-
-    // Subtract the value of those quarters from cents
-
-
-    // Calculate how many dimes you should give customer
-    // Subtract the value of those dimes from remaining cents
-
-    // Calculate how many nickels you should give customer
-    // Subtract the value of those nickels from remaining cents
-
-    // Calculate how many pennies you should give customer
-    // Subtract the value of those pennies from remaining cents
-
-    // Sum the number of quarters, dimes, nickels, and pennies used
-    // Print that sum
-
