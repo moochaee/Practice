14e6df8bf4539912845b99030fe29c0cdd2c4a8a
moochaee <marcusamad@live.com>
2024-10-03 03:56:17 +0000

    Thu, Oct 3, 2024, 1:56 PM +10:00

--- calc.c
+++ calc.c
@@ -5,12 +5,11 @@ int main(void)
 {
 int x = get_int("x: ");
 int y = get_int("y: ");
-int z = add(x,y);
-printf("%i\n", z);
+
+printf("%i\n", add(x,y));
 }
 
 int add(int a, int b)
 {
-    int c = a + b;
-    return c;
+    return a + b;
 }
