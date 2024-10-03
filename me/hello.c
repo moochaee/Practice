855e4e90cb226d2b8bde21fd4ac4544ddca4ed1d
moochaee <marcusamad@live.com>
2024-10-03 05:51:46 +0000

    Thu, Oct 3, 2024, 3:51 PM +10:00

--- me/hello.c
+++ me/hello.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <cs50.h>
-
+#include <stdio.h>
+// ask for name
 int main(void)
 {
     string name = get_string("What's your name?\n");
-
+    // greet the user by name
     printf("hello, %s\n", name);
 }
