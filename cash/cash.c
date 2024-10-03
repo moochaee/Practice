35b04af239e3f828cfbc3bca40f9f133ab3e037f
moochaee <marcusamad@live.com>
2024-10-03 08:20:39 +0000

    Thu, Oct 3, 2024, 6:20 PM +10:00

--- cash/cash.c
+++ cash/cash.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int calculate_quarters(int cents);
-
+int calculate_dimes(int cents);
 
 int main(void)
 {
@@ -37,3 +37,14 @@ int calculate_quarters(int cents)
     int dimes = calculate_dimes(cents);
     // Subtract the value of those dimes from remaining cents
     cents = cents - (dimes * 10);
+
+int calculate_dimes(int cents)
+{
+    // Calculate how many dimes you should give customer
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes++;
+        cents = cents -10;
+    }
+}
