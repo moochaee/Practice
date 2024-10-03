0ea026a2fb8e7e62ccec95bfb56605ab594bb845
moochaee <marcusamad@live.com>
2024-10-03 07:47:30 +0000

    Thu, Oct 3, 2024, 5:47 PM +10:00

--- cash/cash.c
+++ cash/cash.c
@@ -3,5 +3,26 @@
 
 int main(void)
 {
-
+    // Prompt the user for change owed, in cents
+    int cents;
+    do
+    {
+    cents = get_int("Change owed ");
+    }
+    while (cents < 0);
 }
+    // Calculate how many quarters you should give customer
+    // Subtract the value of those quarters from cents
+
+    // Calculate how many dimes you should give customer
+    // Subtract the value of those dimes from remaining cents
+
+    // Calculate how many nickels you should give customer
+    // Subtract the value of those nickels from remaining cents
+
+    // Calculate how many pennies you should give customer
+    // Subtract the value of those pennies from remaining cents
+
+    // Sum the number of quarters, dimes, nickels, and pennies used
+    // Print that sum
+
