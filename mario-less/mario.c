b0cbbd0c9997dc28c8c4c1b380df73cc807be0dd
moochaee <marcusamad@live.com>
2024-10-03 06:12:53 +0000

    Thu, Oct 3, 2024, 4:12 PM +10:00

---
+++ mario-less/mario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_row(int bricks);
+
+int main(void)
+{
+    // Prompt the user for the pyramid's height
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+
+    // Print a pyramid of that height
+    for (int i = 0; i < n; i++)
+    {
+        //Print row of bricks
+        print_row(i + 1);
+    }
+}
+
+void print_row(int bricks)
+{
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
