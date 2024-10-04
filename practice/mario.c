978259ad4a12c394880df5e91350e5fd13d83d0f
moochaee <marcusamad@live.com>
2024-10-04 06:45:26 +0000

    Fri, Oct 4, 2024, 4:45 PM +10:00

--- practice/mario.c
+++ practice/mario.c
@@ -1,17 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_row(int length);
 int main(void)
 
 {
     //Get height from user
     int height = get_int("Height ");
+    print_row(4);
 
 }
 
 void print_row(int length)
 {
-    for (int i = 0, i < length, i++)
+    for (int i = 0; i < length; i++)
     {
         printf("#");
     }
