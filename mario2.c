107aefdd02de1d310b02913c3d57e5829919b279
moochaee <marcusamad@live.com>
2024-10-04 23:19:11 +0000

    Sat, Oct 5, 2024, 9:19 AM +10:00

---
+++ mario2.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int length);
+int main(void)
+
+{
+    //Get height from user
+    int height = get_int("Height ");
+
+    for (int i = 0; i < height; i++)
+    {
+    print_row(i + 1);
+    }
+}
+//print row of user defined #
+void print_row(int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
