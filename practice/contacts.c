f552dada4dc2fe27345ecc0a049e14df674737a0
moochaee <marcusamad@live.com>
2024-10-04 06:13:15 +0000

    Fri, Oct 4, 2024, 4:13 PM +10:00

---
+++ practice/contacts.c
@@ -0,0 +1,14 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+
+{
+    string name = get_string("What is your name? ");
+    int age = get_int("What is your age? ");
+    string phone_number = get_string("What is your phone number? ");
+
+    printf("Name: %s\n", name);
+    printf("Age: %i\n", age);
+    printf("Number: %s\n", phone_number);
+}
