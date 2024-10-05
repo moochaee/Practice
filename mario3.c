e33026c9125c4b3a0cd4fcff581a4cf3e00e4a75
moochaee <marcusamad@live.com>
2024-10-05 00:19:15 +0000

    Sat, Oct 5, 2024, 10:19 AM +10:00

---
+++ mario3.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int length);
+int main(void)
+
+{
+    printf("Program started\n"); // Debugging line
+    //Get height from user
+    int height = get_int("Height ");
+    printf("Height entered: %i\n", height); // Debugging line
+
+    for (int i = 0; i < height; i++)
+{
+    printf("i: %i\n", i); // Debugging line
+    print_row(i + 1);
+}
+}
+//print row of user defined # test
+void print_row(int length)
+{
+    printf("length: %i\n", length); // Debugging line
+    for (int i = 0; i < length; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
