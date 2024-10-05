8108bbd83f726de6ecd42d0da4a754a693f74cfe
moochaee <marcusamad@live.com>
2024-10-05 21:57:58 +0000

    Sun, Oct 6, 2024, 8:57 AM +11:00

--- scores.c
+++ scores.c
@@ -3,14 +3,14 @@
 
 int main(void)
 {
-    const int n = 3
-    int scores[n];
-    for (int i = 0; i < n; i++)
+    const int N = 3;
+    int scores[N];
+    for (int i = 0; i < N; i++)
     {
     scores[i] = get_int("Score: ");
     }
 
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) n);
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
 
 }
