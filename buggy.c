6299cadeb37570f80496e3b63ff33f5a9b1648d1
moochaee <marcusamad@live.com>
2024-10-05 08:06:56 +0000

    Sat, Oct 5, 2024, 6:06 PM +10:00

--- buggy.c
+++ buggy.c
@@ -2,9 +2,14 @@
 
 int main(void)
 {
-    for (int i = 0; i <3; i++)
-    {
-        printf("#\n");
-    }
+    int h = get_int("Height: ");
+    print_column(h)
+}
 
+void print_column(int height)
+{
+    for int i = 0; i <= height; i++)
+    {
+        printf("#\n";);
+    }
 }
