b816f40e2085eed2e1ff2c9e3cf84812c57284ce
moochaee <marcusamad@live.com>
2024-10-07 04:35:41 +0000

    Mon, Oct 7, 2024, 3:35 PM +11:00

--- practice/alphabetical.c
+++ practice/alphabetical.c
@@ -1,8 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string phrase = get_string("Enter a phrase: ");
-    
+    int length = strlen(phrase);
+    for (int i = 0; i < length, i++);
 }
