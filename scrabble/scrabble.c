6626977087741bf6c8ba262eaf719816833f9a4e
moochaee <marcusamad@live.com>
2024-10-07 05:38:01 +0000

    Mon, Oct 7, 2024, 4:38 PM +11:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for two words
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Compute the score of each word
+
+    // Print the winner
+}
