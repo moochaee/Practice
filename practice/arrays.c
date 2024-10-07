60c6aafb3737077e1ea5e807d486c1fcbb41aa71
moochaee <marcusamad@live.com>
2024-10-07 04:24:04 +0000

    Mon, Oct 7, 2024, 3:24 PM +11:00

--- practice/arrays.c
+++ practice/arrays.c
@@ -3,12 +3,13 @@
 
 int main(void)
 {
-    int sequences[5];
+    int size = 9;
+    int sequences[size];
 
     sequences[0] = 1;
     printf("%i\n", sequences[0]);
 
-    for (int i = 1; i < 5; i++)
+    for (int i = 1; i < size; i++)
     {
         sequences[i] = sequences[i-1] * 2;
         printf("%i\n", sequences[i]);
