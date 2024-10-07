495b4928bdc4634b6730e7abd34b77f58f1ed916
moochaee <marcusamad@live.com>
2024-10-07 06:07:33 +0000

    Mon, Oct 7, 2024, 5:07 PM +11:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -10,8 +10,9 @@ int main(void)
     string word2 = get_string("Player 2: ");
 
     // Compute the score of each word
-
-    int scores[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    int array = 26;
+    int scores[array] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    for (int i = 0; i < array; i++)
 
     // Print the winner
 }
