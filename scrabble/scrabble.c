c6ca30ec542540e0dd581cd97546e1384e5bcbe2
moochaee <marcusamad@live.com>
2024-10-08 05:03:21 +0000

    Tue, Oct 8, 2024, 4:03 PM +11:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -9,6 +9,9 @@ int main(void)
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
+    //Convert each character to uppercase
+
+
     // Compute the score of each word
     int array = 26;
     int scores[array] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
