c531cdb5a184ded01144695636353f1000b89f9b
moochaee <marcusamad@live.com>
2024-10-08 05:29:16 +0000

    Tue, Oct 8, 2024, 4:29 PM +11:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,7 @@ int main(void)
     // Compute the score of each word
     int array = 26;
     int scores[array] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
     scores[word1[i] - 'A'];
 
     // Print the winner
