af63ab1314be2814793f7d554be2fec786729470
moochaee <marcusamad@live.com>
2024-10-09 21:41:21 +0000

    Thu, Oct 10, 2024, 8:41 AM +11:00

---
+++ caesar/caesar.c
@@ -0,0 +1,14 @@
+int main(int argc, string argv[])
+{
+    // Make sure program was run with just one command-line argument
+
+    // Make sure every character in argv[1] is a digit
+
+    // Convert argv[1] from a `string` to an `int`
+
+    // Prompt user for plaintext
+
+    // For each character in the plaintext:
+
+        // Rotate the character if it's a letter
+}
