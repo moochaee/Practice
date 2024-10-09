d60c31fae06ef56025233c54788936d7a42435b8
moochaee <marcusamad@live.com>
2024-10-09 03:28:24 +0000

    Wed, Oct 9, 2024, 2:28 PM +11:00

---
+++ readability/readability.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for some text
+    string input = get_string("Text: "/n);
+}
+
+    // Count the number of letters, words, and sentences in the text
+
+    // Compute the Coleman-Liau index
+
+    // Print the grade level
+
+
