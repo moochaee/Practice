10db2652c7a0bb5be35b874aae91fd1868de4c7b
moochaee <marcusamad@live.com>
2024-10-14 06:36:32 +0000

    Mon, Oct 14, 2024, 5:36 PM +11:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -126,6 +126,10 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     // TODO
     return false;
 }
