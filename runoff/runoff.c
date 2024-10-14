5443662a48a4602f7358bebd2b5ce04e2b8eead1
moochaee <marcusamad@live.com>
2024-10-14 06:44:38 +0000

    Mon, Oct 14, 2024, 5:44 PM +11:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,6 +131,7 @@ bool vote(int voter, int rank, string name)
         if(strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
+            return true;
 
         }
     }
