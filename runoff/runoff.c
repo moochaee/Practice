3321db9602be4e0f38017c12058297d4f20c1c3d
moochaee <marcusamad@live.com>
2024-10-15 06:14:21 +0000

    Tue, Oct 15, 2024, 5:14 PM +11:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -161,6 +161,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
+    If(candidates[i].votes > (voter_count)/2)
+    {
+        printf("candidates[i].name/n")
+    }
     // TODO
     return false;
 }
