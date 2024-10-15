d0a311622a28b247b8593d4f049a5d6d7eebf175
moochaee <marcusamad@live.com>
2024-10-15 06:23:16 +0000

    Tue, Oct 15, 2024, 5:23 PM +11:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -176,6 +176,15 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+
+    int lowest_votes = 100;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < lowest_votes || candidates[i].eliminated == false)
+        {
+            lowest_votes = candidates[i].votes;
+        }
+    }
     // TODO
     return 0;
 }
