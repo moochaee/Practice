9ddab368c1c6869e7d597d6945dc1827d8078c7d
moochaee <marcusamad@live.com>
2024-10-15 06:02:35 +0000

    Tue, Oct 15, 2024, 5:02 PM +11:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,7 +145,7 @@ void tabulate(void)
 {
     for(int i = 0; i < voter_count; i++)
     {
-        for(int j = 0; j < preferences[j][0]; j++)
+        for(int j = 0; j < candidate_count; j++)
     }
     {
         if(candidates[i].eliminated == false)
