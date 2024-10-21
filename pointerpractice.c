35d60908135770740cfc0e6112ae27873944272b
moochaee <marcusamad@live.com>
2024-10-21 06:18:38 +0000

    Mon, Oct 21, 2024, 5:18 PM +11:00

--- pointerpractice.c
+++ pointerpractice.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <cs50.h>
+
+void swap(int *a, int *b);
 
 int main(void)
 {
-    int calls = 8;
-    int *p = &calls;
+    int a = 10;
+    int b = 50;
 
     {
-    printf("%i\n", *p);
+    printf("%i %i\n", a, b);
+    swap(&a, &b);
+    printf("%i %i\n", a, b);
     }
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
