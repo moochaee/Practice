96b5395d9072509ecc12941b216f996479d15674
moochaee <marcusamad@live.com>
2024-10-24 06:21:29 +0000

    Thu, Oct 24, 2024, 5:21 PM +11:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -136,32 +136,3 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 }
-
-
-
-
-
-
-
-            if(i - 1 >= 0)
-            {
-                copy[i-1][j];
-            }
-            else if(j - 1 >= 0)
-            {
-                copy[i][j - 1];
-            }
-            else if(i + 1 < height)
-            {
-                copy[i + 1][j]
-            }
-            else if(j + 1 < width)
-            {
-                copy[i][j + 1];
-            }
-
-
-            int RGBred = round(copy[i - 1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j - 1].rgbtRed + copy[i][j].rgbtRed + copy[i][j + 1].rgbtRed + copy[i + 1][j - 1].rgbtRed + copy[i + 1][j].rgbtRed + copy[i + 1][j + 1].rgbtRed) / 9.0;
-            int RGBGreen = RGBGreen = round(copy[i - 1][j-1].rgbtGreen + copy[i-1][j].rgbtGreen + copy[i-1][j+1].rgbtGreen + copy[i][j - 1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j + 1].rgbGreen + copy[i + 1][j - 1].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i + 1][j + 1].rgbtGreen) / 9.0;
-            int RGBBlue = RGBBlue = round(copy[i - 1][j-1].rgbtBlue + copy[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + copy[i][j - 1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j + 1].rgbtBlue + copy[i + 1][j - 1].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i + 1][j + 1].rgbtBlue) / 9.0;
-            image[i][j] = copy[i][j];
