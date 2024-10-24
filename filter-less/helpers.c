c32d62ea2769b8fe1ec81dc814d14cb04d983c86
moochaee <marcusamad@live.com>
2024-10-24 04:24:00 +0000

    Thu, Oct 24, 2024, 3:24 PM +11:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -81,6 +81,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            while(i = )
             copy[i][j] = image[i][j];
         }
     }
