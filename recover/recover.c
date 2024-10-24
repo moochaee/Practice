f7b225b04808927f4ffc5835450a3487801a6c10
moochaee <marcusamad@live.com>
2024-10-24 23:52:11 +0000

    Fri, Oct 25, 2024, 10:52 AM +11:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,23 +28,22 @@ int main(int argc, char *argv[])
     int file_number = 0;
     bool is_writing = false;
     FILE *img = NULL;
+    filename[FILENAME_SIZE];
 
-
-    // While there's still data left to read from the memory card
-    while (fread(buffer, 1, BLOCK_SIZE, card) == BLOCK_SIZE)
-        {
-        //Check for JPEG
-        if (buffer[0] == 0xff || buffer[1] == 0xd8 || buffer[2] == 0xff || (buffer[3] < 0xe0 || buffer[3] > 0xef))
-        {if (img != NULL) {
-        fclose(img);
-    }
-    // Create new file
-    sprintf(filename, "%03i.jpg", file_number++);
-    img = fopen(filename, "w");
-    is_writing = true;
-}
-
-if (is_writing) {
-    fwrite(buffer, 1, BLOCK_SIZE, img);
-}
+// While there's still data left to read from the memory card
+while (fread(buffer, 1, BLOCK_SIZE, card) == BLOCK_SIZE) {
+    // Check for JPEG
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
+        if (img != NULL) {
+            fclose(img);
         }
+        // Create new file
+        sprintf(filename, "%03i.jpg", file_number++);
+        img = fopen(filename, "w");
+        is_writing = true;
+    }
+
+    if (is_writing) {
+        fwrite(buffer, 1, BLOCK_SIZE, img);
+    }
+}
