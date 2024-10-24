cb691451410651e19887d5a03a5acb5850cf67c4
moochaee <marcusamad@live.com>
2024-10-24 05:12:51 +0000

    Thu, Oct 24, 2024, 4:12 PM +11:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -82,7 +82,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         copy[i][j] = image[i][j];
         {
-            if(i >= 0 && i < (height - 1) && j >= 0 && j < (width - 1))
+            if(i - 1 < i[0]
             {
             int RGBred = round(copy[i - 1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j - 1].rgbtRed + copy[i][j].rgbtRed + copy[i][j + 1].rgbtRed + copy[i + 1][j - 1].rgbtRed + copy[i + 1][j].rgbtRed + copy[i + 1][j + 1].rgbtRed) / 9.0;
             int RGBGreen = RGBGreen = round(copy[i - 1][j-1].rgbtGreen + copy[i-1][j].rgbtGreen + copy[i-1][j+1].rgbtGreen + copy[i][j - 1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j + 1].rgbGreen + copy[i + 1][j - 1].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i + 1][j + 1].rgbtGreen) / 9.0;
