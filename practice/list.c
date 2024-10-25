1810fa138ed9c1b491810ad984601d776834e4c3
moochaee <marcusamad@live.com>
2024-10-25 03:49:58 +0000

    Fri, Oct 25, 2024, 2:49 PM +11:00

--- practice/list.c
+++ practice/list.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(void)
 {
-    int list[3];
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
 
     list[0] = 1;
     list[1] = 2;
