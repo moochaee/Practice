bc266f6b48695f85773bd5ca1fb2e2408f7d80af
moochaee <marcusamad@live.com>
2024-10-25 05:25:57 +0000

    Fri, Oct 25, 2024, 4:25 PM +11:00

---
+++ list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+} node;
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+}
