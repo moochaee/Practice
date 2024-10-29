9efecbaf046bccbc2216fce1846954e9472e02b5
moochaee <marcusamad@live.com>
2024-10-29 05:24:06 +0000

    Tue, Oct 29, 2024, 4:24 PM +11:00

--- practice/list.c
+++ practice/list.c
@@ -1,38 +1,58 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+} node;
+
+#define LIST_SIZE 2
+
+bool unload(node *list);
+void visualizer(node *list);
 
 int main(void)
 {
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
+    node *list = NULL;
+
+    // Add items to list
+    for (int i = 0; i < LIST_SIZE; i++)
     {
+        string phrase = get_string("Enter a new phrase: ");
+
+        // TODO: add phrase to new node in list
+
+        // Visualize list after adding a node.
+        visualizer(list);
+    }
+
+    // Free all memory used
+    if (!unload(list))
+    {
+        printf("Error freeing the list.\n");
         return 1;
     }
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = malloc(4 * sizeof(int));
-    if (tmp == NULL)
-    {
-        free(list);
-        return 1;
-    }
-
-    for (int i = 0; i < 3; i++)
-    {
-        tmp[i] = list[i];
-    }
-    tmp[3] = 4;
-
-    free (list);
-    list = tmp;
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-    free(list);
+    printf("Freed the list.\n");
     return 0;
+}
+
+bool unload(node *list)
+{
+    // TODO: Free all allocated nodes
+    return false;
+}
+
+void visualizer(node *list)
+{
+    printf("\n+-- List Visualizer --+\n\n");
+    while (list != NULL)
+    {
+        printf("Location %p\nPhrase: \"%s\"\nNext: %p\n\n", list, list->phrase, list->next);
+        list = list->next;
+    }
+    printf("+---------------------+\n\n");
 }
