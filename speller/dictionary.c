c6a2ea019e5a412f5a9704ea5f80c7ab1190ac0d
moochaee <marcusamad@live.com>
2024-10-31 03:13:04 +0000

    Thu, Oct 31, 2024, 2:13 PM +11:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -57,8 +57,8 @@ bool load(const char *dictionary)
         {
         node *n = malloc(sizeof(node));
         strcpy(n->word, buffer);
-        n->next = table[N];
-        table[N] = n;
+        n->next = table[hash];
+        table[hash] = n;
         }
         is_loaded = true
         return true;
