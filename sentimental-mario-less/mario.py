a0d207a1f540b274f467a7e26e66c67e13951a0f
moochaee <marcusamad@live.com>
2024-11-12 02:09:15 +0000

    Tue, Nov 12, 2024, 1:09 PM +11:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,12 +1,15 @@
 while True:
+    try
     height = int(input("Height: "))
 
     if height > 0 and height <9:
         break
-    
+
 for i in range(height):
     print(f"{' ' * (height - i - 1)}{'#' * (i + 1)}")
 
+    except ValueError:
+        height = int(input("Height: "))
 
 
 
