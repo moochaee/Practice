ca35d27191911a6fc834c6ca3bd5c49c15a724e5
moochaee <marcusamad@live.com>
2024-11-26 07:38:31 +0000

    Tue, Nov 26, 2024, 6:38 PM +11:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,11 +1,15 @@
 from cs50 import get_float
 
+
+
 while True:
     try:
+        # get user input for change
         change = get_float("Change: ")
+        #check that it is a non-negative integer
         if change > 0.00:
             break
     except ValueError:
-
+        pass
 
 
