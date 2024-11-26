167abcd39be472a9a10030a0c30668d1144ea4a4
moochaee <marcusamad@live.com>
2024-11-26 08:05:17 +0000

    Tue, Nov 26, 2024, 7:05 PM +11:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,7 +4,7 @@ text = input("Text: ")
 
 letters = 0
 for letter in text:
-    if text.isalpha():
+    if letter.isalpha():
         letters += 1
 
 words = 0
@@ -12,3 +12,7 @@ for word in text:
     if text[word] = " ":
         words += 1
 
+sentences = 0
+for sentence in text:
+    if text[sentence] = "." or text[sentence] = "!" or text[sentence] = "?":
+        sentence += 1
