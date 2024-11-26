0032668047943f25a66284b3744b2d7ca2363a0b
moochaee <marcusamad@live.com>
2024-11-26 08:19:32 +0000

    Tue, Nov 26, 2024, 7:19 PM +11:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -17,8 +17,8 @@ for letter in text:
     if letter == "." or letter == "!" or letter == "?":
         sentences += 1
 
-L = round(((letters/words) *100), 2)
-S = round(((sentences/words) *100), 2)
+L = round(((letters/words) * 100), 2)
+S = round(((sentences/words) * 100), 2)
 
 CLS = round(0.0588 * L - 0.296 * S - 15.8)
 
