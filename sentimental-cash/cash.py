6d34fd69932a6e5fe9882f60bc2deb8707cdb5a7
moochaee <marcusamad@live.com>
2024-11-26 07:50:32 +0000

    Tue, Nov 26, 2024, 6:50 PM +11:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -14,21 +14,21 @@ while True:
 quarters = 0
 while change >= 0.25:
     quarters += 1
-    change = round(change, 2) - 25
+    change = round(change - .25, 2)
 
 dimes = 0
 while change >= 0.10:
     dimes += 1
-    change = round(change, 2) - .10
+    change = round(change- .10, 2)
 
 nickels = 0
 while change >= 0.05:
     nickels += 1
-    change = round(change, 2) - .05
+    change = round(change- .05, 2)
 
 pennies = 0
 while change >= 0.01:
     pennies += 1
-    change = round(change, 2) - .01
+    change = round(change- .01, 2)
 
 print(quarters + dimes + nickels + pennies)
