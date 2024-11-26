63d3a7b7f6c2a68e5d61a1a65d054ad9267df4a3
moochaee <marcusamad@live.com>
2024-11-26 07:52:15 +0000

    Tue, Nov 26, 2024, 6:52 PM +11:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,7 +5,7 @@ while True:
     try:
         # get user input for change
         change = get_float("Change: ")
-        #check that it is a non-negative integer
+        # check that it is a non-negative integer
         if change > 0.00:
             break
     except ValueError:
@@ -19,16 +19,16 @@ while change >= 0.25:
 dimes = 0
 while change >= 0.10:
     dimes += 1
-    change = round(change- .10, 2)
+    change = round(change - .10, 2)
 
 nickels = 0
 while change >= 0.05:
     nickels += 1
-    change = round(change- .05, 2)
+    change = round(change - .05, 2)
 
 pennies = 0
 while change >= 0.01:
     pennies += 1
-    change = round(change- .01, 2)
+    change = round(change - .01, 2)
 
 print(quarters + dimes + nickels + pennies)
