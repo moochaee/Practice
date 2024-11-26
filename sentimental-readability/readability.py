9146517e3f83659f4c0e43a168402bfd93b21fe0
moochaee <marcusamad@live.com>
2024-11-26 08:01:43 +0000

    Tue, Nov 26, 2024, 7:01 PM +11:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,3 +2,13 @@ from cs50 import get_string
 
 text = input("Text: ")
 
+letters = 0
+for letter in text:
+    if text.isalpha():
+    letters += 1
+
+words = 0
+for word in text:
+if text[word] = "":
+    words += 1
+
