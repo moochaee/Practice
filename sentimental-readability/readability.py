80e7637c7498684b74f5905bda659e9c769d79fe
moochaee <marcusamad@live.com>
2024-11-26 08:07:03 +0000

    Tue, Nov 26, 2024, 7:07 PM +11:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,11 +8,11 @@ for letter in text:
         letters += 1
 
 words = 0
-for word in text:
-    if text[word] = " ":
+for letter in text:
+    if letter = " ":
         words += 1
 
 sentences = 0
-for sentence in text:
-    if text[sentence] = "." or text[sentence] = "!" or text[sentence] = "?":
-        sentence += 1
+for letter in text:
+    if letter = "." or letter = "!" or letter = "?":
+        sentences += 1
