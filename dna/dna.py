1662704edd66cba62a3946d7fce51175ee8d97f8
moochaee <marcusamad@live.com>
2024-11-27 00:11:19 +0000

    Wed, Nov 27, 2024, 11:11 AM +11:00

--- dna/dna.py
+++ dna/dna.py
@@ -23,7 +23,7 @@ def main():
         dna_sequence = dnafile.read().strip()
 
     # List of STRs
-    STRList = ["AGAT" , "AATG" , "TATC"]
+    STRList = ["AGAT" , "AATG" , "TATC" , "AGATC", "TTTTTTCT" , "TCTAG" , "GATA" , "GAAA" , "TCTG"]
 
     # TODO: Find longest match of each STR in DNA sequence
     for STR in STRList:
