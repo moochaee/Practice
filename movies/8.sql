08ade0208db3cec4caec642fe34ff75e18c9e0ac
moochaee <marcusamad@live.com>
2024-12-10 07:10:02 +0000

    Tue, Dec 10, 2024, 6:10 PM +11:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,6 +1,6 @@
-sqlite> SELECT name
-   ...> FROM people
-   ...> JOIN stars ON people.id = stars.person_id
-   ...> JOIN movies ON movies.id = stars.movie_id
-   ...> WHERE movies.title = 'Toy Story'
-   ...> ;
+SELECT name
+FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movies.title = 'Toy Story'
+;
