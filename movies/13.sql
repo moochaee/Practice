20416bf84878beafcb10505eaa81e8ff009c9703
moochaee <marcusamad@live.com>
2024-12-10 22:58:46 +0000

    Wed, Dec 11, 2024, 9:58 AM +11:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,10 +1,10 @@
-SELECT name
+SELECT co_star.name
 FROM people
 JOIN stars AS star1 ON movies.id = star1.movie_id
 JOIN people AS kevin ON star1.person_id = kevin.id
 JOIN stars AS star2 ON movies.id = star2.movie_id
 JOIN people AS co_star ON star2.person_id = co_star.id
-JOIN movies ON movies.id = stars.movie_id
+JOIN movies ON movies.id = star1.movie_id
 WHERE kevin.name = 'Kevin Bacon'
 AND co_star.name != 'Kevin Bacon'
 ;
