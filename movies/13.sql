8fa21e28fb03550deab451e14219a7616ad5a5c9
moochaee <marcusamad@live.com>
2024-12-10 23:57:16 +0000

    Wed, Dec 11, 2024, 10:57 AM +11:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,7 +4,6 @@ JOIN stars AS star1 ON movies.id = star1.movie_id
 JOIN people AS kevin ON star1.person_id = kevin.id
 JOIN stars AS star2 ON movies.id = star2.movie_id
 JOIN people AS co_star ON star2.person_id = co_star.id
-JOIN movies ON movies.id = star1.movie_id
 WHERE kevin.name = 'Kevin Bacon'
 AND kevin.birth = '1958'
 AND co_star.name != 'Kevin Bacon'
