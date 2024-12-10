09fe4d0a9cedd4704ca17028d81cdc646d7197b0
moochaee <marcusamad@live.com>
2024-12-10 22:36:06 +0000

    Wed, Dec 11, 2024, 9:36 AM +11:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,7 +3,7 @@ FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN ratings ON movies.id = ratings.movie_id
 JOIN people ON people.id = stars.person_id
-WHERE person.id = 'Chadwick Boseman'
+WHERE people.name = 'Chadwick Boseman'
 ORDER BY ratings.rating DESC
 LIMIT 5
 ;
