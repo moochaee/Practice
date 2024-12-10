053e223d7d0376899f522bbd491a6180e49a487b
moochaee <marcusamad@live.com>
2024-12-10 07:07:25 +0000

    Tue, Dec 10, 2024, 6:07 PM +11:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,6 +1,6 @@
 sqlite> SELECT name
    ...> FROM people
-   ...> JOIN stars ON id = stars.person_id
-   ...> JOIN movies.id = stars.movie_id
+   ...> JOIN stars ON people.id = stars.person_id
+   ...> JOIN movies ON movies.id = stars.movie_id
    ...> WHERE movies.name = 'Toy Story'
    ...> ;
